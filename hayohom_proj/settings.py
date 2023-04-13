d32b19bbce3f60bbac0fa570ba9bba8a96d6991d
Emtenan <emtenan.ag@gmail.com>
2023-04-13 14:31:32 +0300

    commit

--- hayohom_proj/settings.py
+++ hayohom_proj/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,7 +87,6 @@ DATABASES = {
     }
 }
 
-django_heroku.settings(locals())
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
